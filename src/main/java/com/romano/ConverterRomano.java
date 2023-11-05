719d0dbf617e74844a78a39956f8d2d3bd1ce824
agcarvalho-eng <agcarvalho@ifto.edu.br>
2023-11-05 10:21:22 -0300

    Troca da versão do Junit

--- src/main/java/com/romano/ConverterRomano.java
+++ src/main/java/com/romano/ConverterRomano.java
@@ -86,24 +86,16 @@ public class ConverterRomano {
 
     //Método para pegar o valor em decimal.
     private static int pegarValorDecimal(char caracterRomano) {
-        switch (caracterRomano) {
-            case 'I':
-                return 1;
-            case 'V':
-                return 5;
-            case 'X':
-                return 10;
-            case 'L':
-                return 50;
-            case 'C':
-                return 100;
-            case 'D':
-                return 500;
-            case 'M':
-                return 1000;
-            default:
-                return -1; // Caracter romano inválido
-        }
+        return switch (caracterRomano) {
+            case 'I' -> 1;
+            case 'V' -> 5;
+            case 'X' -> 10;
+            case 'L' -> 50;
+            case 'C' -> 100;
+            case 'D' -> 500;
+            case 'M' -> 1000;
+            default -> -1; // Caracter romano inválido
+        };
     }
 
     private static boolean romanoValido(String romano) {
